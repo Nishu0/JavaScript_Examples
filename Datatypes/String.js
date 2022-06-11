390b0e4f66dac79bfbada12a7b2828fd245e9a12
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2022-06-11 14:21:16 +0530

    Create String.js

---
+++ Datatypes/String.js
@@ -0,0 +1,8 @@
+console.log("------------");
+console.log("String");
+var str1="My name is Nisarg.";
+console.log(str1);
+var mixedStr='My name is "Nisarg".';
+console.log(mixedStr);
+var escapeStr="My name is \"Nisarg\".";
+console.log(escapeStr);
