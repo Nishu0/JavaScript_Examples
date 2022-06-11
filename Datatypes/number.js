8411eb10593a50adac3d988b13c9e2057f148447
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2022-06-11 14:21:36 +0530

    Add files via upload

---
+++ Datatypes/number.js
@@ -0,0 +1,15 @@
+
+console.log("------------");
+console.log("Numbers");
+var num=97;
+console.log("Roll Number::"+num);
+var int1=-3.14159265358;
+console.log("Pie Negative Value::"+int1);
+var largeInt=97e6;
+console.log("Number with Scientific Notation::"+largeInt);
+var posinf=97/0;
+console.log("Positive Infinity(31/0)::"+posinf);
+var neginf=-97/0;
+console.log("Positive Infinity(-31/0)::"+neginf);
+var nannum="abc"/3;
+console.log("Not a Number::"+nannum);
