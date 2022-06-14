0f307dcbf1f09938b7f9ae0b62cc4088d40e7978
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2022-06-14 13:00:54 +0530

    Create promise_setTimeout_reqAnime.js

---
+++ funky_js/promise_setTimeout_reqAnime.js
@@ -0,0 +1,23 @@
+function foo() {
+			console.log('foo');
+		}
+		function bar(){
+			setTimeout(foo);
+			console.log('bar');
+		}
+		function baz(){
+			setTimeout(()=>console.log('baz'));
+		}
+		function liz(){
+			requestAnimationFrame(()=>console.log('liz'));
+		}
+		function rep(){
+			Promise.resolve().then(()=>console.log('rep'));
+		}
+		console.log('start');
+		foo();
+		bar();
+		baz();
+		liz();
+		rep();
+		console.log('end');
